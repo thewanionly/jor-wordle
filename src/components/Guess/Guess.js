5edaf5f2344e10d2b458a145c4f4e98fed4f8e0f
thewanionly <pelloani@gmail.com>
2023-09-29 03:46:06 +0800

    feat: implement Game logic
    show appropriate cell styling depending on the letter's status

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { range } from '../../utils';
 import { GUESS_STR_LENGTH } from '../../constants';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ value = '' }) {
+function Guess({ value = '', answer }) {
+  const cellStatus = checkGuess(value, answer)?.map(({ status }) => status) ?? [];
+
   return (
     <p className='guess'>
       {range(GUESS_STR_LENGTH).map((colIndex) => (
-        <span className='cell' key={colIndex}>
+        <span className={`cell ${cellStatus[colIndex] ?? ''}`} key={colIndex}>
           {value[colIndex]}
         </span>
       ))}
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,11 +3,11 @@ import Guess from '../Guess';
 import { range } from '../../utils';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
-function GuessResults({ guesses }) {
+function GuessResults({ guesses, answer }) {
   return (
     <div className='guess-results'>
       {range(NUM_OF_GUESSES_ALLOWED).map((rowIndex) => (
-        <Guess key={rowIndex} value={guesses[rowIndex]?.value} />
+        <Guess key={rowIndex} value={guesses[rowIndex]?.value} answer={answer} />
       ))}
     </div>
   );
