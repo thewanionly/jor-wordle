bc2254d5210c8ee88523eabfe44a1189e8cf0f20
thewanionly <pelloani@gmail.com>
2023-09-30 03:28:08 +0800

    refactor: match key and value in GAME_STATE

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,6 @@ export const NUM_OF_GUESSES_ALLOWED = 6;
 export const GUESS_STR_LENGTH = 5;
 export const GAME_STATE = {
   PLAYING: 'playing',
-  WINS: 'win',
-  LOSES: 'lose'
+  WINS: 'wins',
+  LOSES: 'loses'
 };
