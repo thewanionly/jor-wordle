c6cbb6b396f6c9549d8bdcb061f5e41d79431268
thewanionly <pelloani@gmail.com>
2023-10-01 00:13:10 +0800

    perf: reduce loops in keyboard keys status logic

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -32,10 +32,10 @@ const KEYBOARD_KEYS_LAYOUT_ARR = Object.values(KEYBOARD_KEYS_LAYOUT);
 
 // Check the status of each letter that are already guessed
 // Return one single array containing all letters that are already guessed
-// Sorted by priority indicated in KEY_STATUS object
 const getGuessedLettersStatus = (guesses, answer) =>
   guesses
     .flatMap((guess) => checkGuess(guess.value, answer))
+    // sort by priority indicated in KEY_STATUS object
     .sort((a, b) => {
       const statusA = KEY_STATUS[a.status].sortOrder;
       const statusB = KEY_STATUS[b.status].sortOrder;
@@ -44,28 +44,42 @@ const getGuessedLettersStatus = (guesses, answer) =>
       if (statusA > statusB) return 1;
 
       return 0;
-    });
+    })
+    // remove duplicates
+    .filter(
+      (value, index, self) =>
+        self.findIndex((selfValue) => selfValue.letter === value.letter) === index
+    );
 
-// Check status of each key
-const checkKeyStatus = (key, guessedLetters) => {
-  // Find the key in the guessedLetters. There might be multiple instances of the key in the guessedLetters
-  // but since `guesedLetters` is sorted with this order: correct -> misplaced -> incorrect,
-  // we will get the first instance according to the order priority.
-  const guessedKey = guessedLetters.find(({ letter }) => letter === key);
+const getAllKeysStatuses = (guesses, answer) => {
+  // Genereate an object containing all key values and statuses
+  const allKeys = Object.fromEntries(
+    Object.values(KEYBOARD_KEYS_LAYOUT)
+      .join('')
+      .split('')
+      .map((k) => [k, { letter: k, status: KEY_STATUS.unused.value }])
+  );
 
-  if (!guessedKey) return KEY_STATUS.unused.value;
+  // Get all guessed letters and their statuses
+  const guessedLetters = getGuessedLettersStatus(guesses, answer);
 
-  return guessedKey.status;
+  // Loop over the guesses and determine the status of each key.
+  // Update the corresponding key in `allKeys` object
+  guessedLetters.forEach(({ letter, status }) => {
+    allKeys[letter].status = status;
+  });
+
+  return allKeys;
 };
 
 const getKeyboardKeys = (guesses, answer) => {
-  // Get all guessed letters and their statuses
-  const guessedLetters = getGuessedLettersStatus(guesses, answer);
+  // Get all keys with their statuses
+  const allKeys = getAllKeysStatuses(guesses, answer);
 
   // Loop over each row. In each row, transform `value` to contain an array of object
   // that containst the key's `letter` and `status`
   return KEYBOARD_KEYS_LAYOUT_ARR.map((value) =>
-    value.split('').map((key) => ({ letter: key, status: checkKeyStatus(key, guessedLetters) }))
+    value.split('').map((key) => ({ letter: key, status: allKeys[key].status }))
   );
 };
 
